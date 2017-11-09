df659138f3d77deaaf43657bee674e8aaeb4b584
DataThinkMonkey <jared.bernard@gmail.com>
2017-11-09 16:15:24 -0600

    First bash scripts and variables.

---
+++ name.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+name="Jared Bernard"
+echo "Hello, $name"
+echo " "
+read -p "How old are you? " age
+echo "$name is $age years old and you are $1."
+echo " "
+read -p "Color: " color
+echo "You like $color"
